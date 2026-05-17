10a662133289a925744f44b98cfc17eaadd38892
lotustemplar <filipetheology@gmail.com>
2026-05-17 11:15:36 -0700

    Redirect pricing import to guarded safe lookup module

--- sitecustomize.py
+++ sitecustomize.py
@@ -4,7 +4,7 @@ import importlib
 import sys
 
 try:
-    live_module = importlib.import_module("pricing_logic_live")
-    sys.modules["pricing_logic_bulk"] = live_module
+    safe_module = importlib.import_module("pricing_logic_safe")
+    sys.modules["pricing_logic_bulk"] = safe_module
 except Exception:
     pass
